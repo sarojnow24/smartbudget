7b9bca625017934f24f0f76a02a8a825ecefb4eb
sarojnow24 <sarojnow24@gmail.com>
2025-12-07 16:24:32 +0400

    Update service-worker.js

--- smartbudget-test/service-worker.js
+++ smartbudget-test/service-worker.js
@@ -1,4 +1,3 @@
-// Change this when releasing a new version
 const CACHE_NAME = "smartbudget-cache-v6";
 
 const urlsToCache = [
@@ -9,15 +8,13 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)).catch(()=>{})
+    caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -27,38 +24,28 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-// Fetch handler
 self.addEventListener("fetch", event => {
-  const url = event.request?.url || "";
-  if (!url.startsWith("http")) return;
-
   if (event.request.mode === 'navigate') {
     event.respondWith(
       fetch(event.request)
-        .then(response => {
-          if (response && response.status === 200) {
-            caches.open(CACHE_NAME).then(c=>c.put(event.request, response.clone()).catch(()=>{}));
-          }
-          return response;
+        .then(res => {
+          caches.open(CACHE_NAME).then(c => c.put(event.request, res.clone()));
+          return res;
         })
-        .catch(()=>caches.match("./offline.html"))
+        .catch(() => caches.match("./offline.html"))
     );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request)
-      .then(response => {
-        if (response && response.status === 200) {
-          caches.open(CACHE_NAME).then(c=>c.put(event.request, response.clone()).catch(()=>{}));
-        }
-        return response;
-      }).catch(()=>cached)
+    caches.match(event.request).then(cached =>
+      cached ||
+      fetch(event.request)
+        .then(res => {
+          caches.open(CACHE_NAME).then(c => c.put(event.request, res.clone()));
+          return res;
+        })
+        .catch(() => cached)
     )
   );
-});
-
-// Skip waiting
-self.addEventListener("message", event => {
-  if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
 });
